eac3fe3fb11ec6f667e89906404c884b21766a65
Vishnu Sreekumar <vishnu.srkmr@gmail.com>
2020-08-11 20:08:55 +0800

    Move the Codebuild resource creation to the main stack. Update the lambda function permissions to give access to the Codebuild project. Remove the obsolete permissions for KMS and SSM access. Update the environment variable list

--- github-pr-auto-build-and-notification/github_pr_auto_build_and_notification/github_pr_auto_build_and_notification_stack.py
+++ github-pr-auto-build-and-notification/github_pr_auto_build_and_notification/github_pr_auto_build_and_notification_stack.py
@@ -1,35 +1,58 @@
-from aws_cdk import core
+from aws_cdk import aws_codebuild as codebuild
+from aws_cdk import aws_apigateway as apigw
 from aws_cdk import aws_lambda as _lambda
 from aws_cdk import aws_iam as iam
-from aws_cdk import aws_apigateway as apigw
+from aws_cdk import core
 
 
 class GithubPrAutoBuildAndNotificationStack(core.Stack):
     def __init__(self, scope, id, **kwargs):
         super().__init__(scope, id, **kwargs)
 
-        # Lambda function to comment on PR
+        # Github oauth token
+        github_token = core.SecretValue.secrets_manager(
+            self.node.try_get_context("github_access_token_path"),
+            json_field="token",
+        )
+
+        # Github source credentials
+        github_creds = codebuild.GitHubSourceCredentials(
+            self, "GithubCredentials", access_token=github_token
+        )
+
+        # Codebuild project to run tests
+        run_tests_project = codebuild.Project(
+            self,
+            "RunTests",
+            badge=True,
+            build_spec=codebuild.BuildSpec.from_source_filename(
+                filename="pipeline/buildspec.yml"
+            ),
+            source=codebuild.Source.git_hub(
+                owner="vissree", repo="testbed", clone_depth=1, webhook=False,
+            ),
+            description="Run the tests",
+            timeout=core.Duration.minutes(10),
+        )
+
+        # Lambda function to trigger the build on PR creation 
         notify_build_start = _lambda.Function(
             self,
             "NotifyBuildStart",
-            handler="notify_build_start.main",
+            handler="start_build.main",
             runtime=_lambda.Runtime.PYTHON_3_8,
             code=_lambda.Code.asset("source"),
             environment={
-                "GITHUB_ACCESS_TOKEN_PATH": self.node.try_get_context(
-                    "github_access_token_path"
-                )
+                "CODE_BUILD_PROJECT_NAME": run_tests_project.project_name
             },
         )
 
-        # Add SSM and KMR access to execution role
-        # to read the Github token from parameter
-        # store
+        # Permission to trigger Codebuild project
         notify_build_start.add_to_role_policy(
             iam.PolicyStatement(
-                actions=["ssm:GetParameter", "kms:Decrypt"],
+                actions=["codebuild:StartBuild"],
                 effect=iam.Effect.ALLOW,
-                resources=["*"],
+                resources=[run_tests_project.project_arn],
             )
         )
 

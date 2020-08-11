91972093fc74972744cad70652c920b29494727a
Vishnu Sreekumar <vishnu.srkmr@gmail.com>
2020-08-11 20:09:31 +0800

    Change the stack references in app.py

--- github-pr-auto-build-and-notification/app.py
+++ github-pr-auto-build-and-notification/app.py
@@ -2,12 +2,10 @@
 
 from aws_cdk import core
 
-#from github_pr_auto_build_and_notification.github_pr_auto_build_and_notification_stack import GithubPrAutoBuildAndNotificationStack
-from codepipeline_and_codebuild.codepipeline_and_codebuild_stack import CodepipelineAndCodebuildStack
+from github_pr_auto_build_and_notification.github_pr_auto_build_and_notification_stack import GithubPrAutoBuildAndNotificationStack
 
 
 app = core.App()
-#GithubPrAutoBuildAndNotificationStack(app, "github-pr-auto-build-and-notification")
-CodepipelineAndCodebuildStack(app, "codepipeline-and-codebuild")
+GithubPrAutoBuildAndNotificationStack(app, "github-pr-auto-build-and-notification")
 
 app.synth()
